7a46ce16be6a8db6364f93b106f91059d69869cf
Onuh Uche <onuhblaze@gmail.com>
2024-02-07 11:44:31 +0100

    first program

---
+++ firstprogram/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Onuh Uche" << std::endl;
+    std::cout << "Onuh Uche" << std::endl;
+    std::cout << "Onuh Uche" << std::endl;
+    std::cout << "Onuh Uche" << std::endl;
+    std::cout << "Onuh Uche" << std::endl;
+    std::cout << "Onuh Uche" << std::endl;
+    std::cout << "Onuh Uche" << std::endl;
+    std::cout << "Onuh Uche" << std::endl;
+    std::cout << "Onuh Uche" << std::endl;
+    std::cout << "Onuh Uche" << std::endl;
+    std::cout << "Onuh Uche" << std::endl;
+}
