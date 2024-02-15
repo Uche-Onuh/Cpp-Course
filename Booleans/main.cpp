893d1b39476f213e28750526d309a8741a0e99ef
Onuh Uche <onuhblaze@gmail.com>
2024-02-15 09:52:07 +0100

    booleans

---
+++ Booleans/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main()
+{
+    bool red_light{true};
+    bool green_light{false};
+
+    if (red_light == true)
+    {
+        std::cout << "Stop!" << std::endl;
+    }
+    else
+    {
+        std::cout << "Go through!" << std::endl;
+    }
+
+    if (green_light)
+    {
+        std::cout << "The light is green!" << std::endl;
+    }
+    else
+    {
+        std::cout << "The light is not green!" << std::endl;
+    }
+
+    std::cout << "sizeof(bool) : " << sizeof(bool) << std::endl;
+
+    std::cout << std::endl;
+    std::cout << std::boolalpha;
+    std::cout << "red light : " << red_light << std::endl;
+    std::cout << "green light : " << green_light << std::endl;
+
+
+    return 0;
+}
