69519295d914b5713c07c8cdc49d99b979ee7d76
Onuh Uche <onuhblaze@gmail.com>
2024-02-16 12:09:04 +0100

    Basic operations

---
+++ 5.2 BasicOperations/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main()
+{
+    // Addition
+    int number1{2};
+    int number2{7};
+
+    int result = number1 + number2;
+    std::cout << "result : " << result << std::endl;
+
+    // subtraction
+    result = number2 - number1;
+    std::cout << "result : " << result << std::endl;
+
+    result = number1 - number2;
+    std::cout << "result : " << result << std::endl;
+
+    // multiplication
+    result = number1 * number2;
+    std::cout << "result : " << result << std::endl;
+
+    // division
+    result = number2 / number1;
+    std::cout << "result : " << result << std::endl;
+
+    // modulus
+    result = number2 % number1;
+    std::cout << "result : " << result << std::endl;
+
+    return 0;
+}
